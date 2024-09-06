bef8dc9457f04f0f49d267eeecdd7d1a200da54a
Alexander González <egonzalez-2019065@kinal.edu.gt>
2024-09-06 10:51:25 -0600

    Cambios necesarios para la comunicación desde el ejecutable al API

--- app/itop_peticion.py
+++ app/itop_peticion.py
@@ -1,48 +1,60 @@
-from flask import Blueprint, jsonify
-from .get_json import get_json
+from flask import Blueprint, jsonify, request
 from config.configuracion import Config
 import requests
+import logging
 
+# Configurar logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Define el Blueprint 
 itop_request = Blueprint('itop_request', __name__)
 
-@itop_request.route('/insert_request')
+@itop_request.route('/insert_request', methods=['POST'])
 def process_json():
     config_instance = Config()
     config_instance.config()
 
-    api_url = config_instance.API_URL
-    api_itop = config_instance.ITOP_URL
+    api_itop = config_instance.ITOP_URL  # API de iTop para insertar datos
 
     try:
-        data = get_json() 
+        # Obtener los datos del cuerpo de la solicitud
+        request_data = request.get_json()
+        logging.info(f"Extracción de la solicitud: {request_data}", )
 
-        # Manejar la respuesta 
-        response = requests.post(api_url)
-        if response.status_code != 200:
-            return jsonify({'error': 'Error al realizar la conexión al API'})
+        # Obtener los datos del header 
+        request_token = request.headers.get('Authorization')
+        if request_token.startswith('Bearer '):
+            request_token = request_token[len('Bearer '):]
+
+        logging.info(f"Extracción del header: {request_token}")
         
-        # Extraer el token de la respuesta 
-        token = response.json().get('token')
-        if not token: 
-            return jsonify({'Error': 'Error al obtener el token'})
-        
+        # Extraer el token y los datos desde la solicitud
+        token = request_token
+        data = request_data
+
+        if not token:
+            logging.info("Token:", token)
+            return jsonify({'error': 'Token no proporcionado'}), 400
+            
+
+        if not data:
+            logging.info("Data:", data)
+            return jsonify({'error': 'Datos no proporcionados'}), 400
+
+        # Definir los headers con el token
         headers = {
             'Authorization': f'Bearer {token}',
             'Content-Type': 'application/json'
         }
 
-        # Enviar los datos a Itop 
+        # Enviar los datos a la API de iTop
         itop_response = requests.post(api_itop, json=data, headers=headers)
 
         # Verificar la respuesta de iTop
         if itop_response.status_code == 200:
             return jsonify({'success': 'Los datos fueron enviados correctamente a iTop'}), 200
         else:
-            return jsonify({'error': 'Error al enviar los datos a iTop', 'details': itop_response.text}), 500
-
-    except FileNotFoundError as e:
-        return jsonify({'El archivo no fue encontrado': e}), 404
-    except ValueError as e:
-        return jsonify({'El archivo no contiene el contenido esperado': e}), 500
+            logging.info(data)
+            return jsonify({'error': 'Error al enviar los datos Itop', 'details': itop_response.text})
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': f'Error en la petición HTTP: {str(e)}'}), 500
